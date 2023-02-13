4005ea89048d633fc8a2d03d9af9c7adf764c593
Matt Dillane <mattdillane3@gmail.com>
2023-02-13 20:47:31 +0000

    Commit

--- EnergyManager.EnergyDataAPI/Controllers/MeterInformationController.cs
+++ EnergyManager.EnergyDataAPI/Controllers/MeterInformationController.cs
@@ -1,12 +1,58 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using EnergyManager.EnergyDataAPI.DTOs;
+using Microsoft.AspNetCore.Mvc;
 
 namespace EnergyManager.EnergyDataAPI.Controllers
 {
-    public class MeterInformationController : Controller
+    [Route("api/MeterInformation")]
+    [ApiController]
+    public class MeterInformationController : ControllerBase
     {
-        public IActionResult Index()
+        [HttpGet("id", Name = "GetDeviceData")]
+        public ActionResult<DeviceDataReadDto> GetDeviceData(Guid deviceId)
         {
-            return View();
+            return Ok();
+        }
+
+        [HttpGet("id", Name = "GetDeviceName")]
+        public ActionResult<string> GetDeviceName(Guid deviceId)
+        {
+            return Ok();
+        }
+
+        [HttpGet("id", Name = "GetDeviceLocation")]
+        public ActionResult<string> GetDeviceLocation(Guid deviceId)
+        {
+            return Ok();
+        }
+
+        [HttpGet("id", Name = "GetDeviceType")]
+        public ActionResult<string> GetDeviceType(Guid deviceId)
+        {
+            return Ok();
+        }
+
+        [HttpGet("id", Name = "GetAllDevices")]
+        public ActionResult<IEnumerable<DeviceDataReadDto>> GetAllDevices(Guid customerId)
+        {
+            return Ok();
+        }
+
+        [HttpPost]
+        public ActionResult<IEnumerable<DeviceDataReadDto>> CreateDevice(Guid customerId)
+        {
+            return Ok();
+        }
+
+        [HttpPost]
+        public ActionResult<IEnumerable<DeviceDataReadDto>> DeleteDevice(Guid deviceId)
+        {
+            return Ok();
+        }
+
+        [HttpPost]
+        public ActionResult<IEnumerable<DeviceDataReadDto>> DisableDevice(Guid deviceId)
+        {
+            return Ok();
         }
     }
 }
